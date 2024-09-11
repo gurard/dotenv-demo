6d340f50481b25c136c149b9253bfc6ebee71e7c
gwaycoder <drew.weber@mckesson.com>
2024-09-11 12:55:33 -0400

    added .env file with sample values, added sample code to consume env variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-const main = async () => {
-  process.exit(0);
+const main = () => {
+  const nanaCount: number = parseInt(process.env.NANA);
+  const drewCount: number = parseInt(process.env.DREW);
+
+  for (let i = 0; i < nanaCount; i++) {
+    console.log('Nana was here');
+  }
+
+  for (let i = 0; i < drewCount; i++) {
+    console.log('Drew was here');
+  }
 };
 
-main().catch((error) => {
-  console.error(error);
-  console.log('job completed with errors');
-  process.exit(-1);
-});
+main();
