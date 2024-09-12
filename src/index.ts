8f2c36a94b234d3d0b0a6845291da785266fd825
gwaycoder <drew.weber@mckesson.com>
2024-09-12 12:12:38 -0400

    simplify example

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 const main = () => {
-  const nanaCount: number = parseInt(process.env.NANA);
-  const drewCount: number = parseInt(process.env.DREW);
+  const helloWorldCount: number = parseInt(process.env.HELLO_WORLD_COUNT);
 
-  for (let i = 0; i < nanaCount; i++) {
-    console.log('Nana was here');
-  }
-
-  for (let i = 0; i < drewCount; i++) {
-    console.log('Drew was here');
+  for (let i = 0; i < helloWorldCount; i++) {
+    console.log('Hello World');
   }
 };
 
